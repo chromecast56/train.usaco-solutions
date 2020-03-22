139fbed1e0c69a6626db5c3c76a7900ce13601a0
chromecast56 <51351043+chromecast56@users.noreply.github.com>
2020-03-21 22:07:12 -0600

    Update evolution.cpp
    comments

--- usaco/evolution.cpp
+++ usaco/evolution.cpp
@@ -7,15 +7,15 @@ typedef pair<int,int> pii;
 #define pb push_back
 typedef vector<int> vi;
 
-vector<unordered_set<string>> cows;
-unordered_map<string,int> sz;
-vector<pair<int,string>> order;
-bool visited[26];
+vector<unordered_set<string>> cows; //array of cows, each w/ a set of traits
+unordered_map<string,int> sz; //store the "subtree sizes", how many times each trait occurs
+vector<pair<int,string>> order; //determine ordering of traits to group by (largest size first)
+bool visited[26]; //visited[i] means "have we seen the trait i?"
 
-bool dfs(vector<unordered_set<string>> &v, int ind = 0) {
-    if (ind==sz.size()) return 1;
+bool dfs(vector<unordered_set<string>> &v, int ind = 0) { //recursively divide the groups by two (ind is the index of the trait in
+    if (ind==sz.size()) return 1;                         //the order vector
 
-    vector<unordered_set<string>> a,b;
+    vector<unordered_set<string>> a,b; //Vec a is the cows that have the trait, Vec b is the cows that don't
 
     for (int i=0;i<v.size();i++)
         if (v[i].find(order[ind].S) != v[i].end())
@@ -23,10 +23,10 @@ bool dfs(vector<unordered_set<string>> &v, int ind = 0) {
         else
             b.push_back(v[i]);
 
-    if (!a.empty() && visited[ind]) return 0;
-    if (!a.empty()) visited[ind] = 1;
+    if (!a.empty() && visited[ind]) return 0; //we saw the trait in this group and in an eariler group, so its bad
+    if (!a.empty()) visited[ind] = 1; //we saw the trait in this group, mark it
 
-    return dfs(a,ind+1) && dfs(b,ind+1);
+    return dfs(a,ind+1) && dfs(b,ind+1); // Recurse down, if any group is bad then the entire system is bad
 }
 int main()
 {
@@ -37,7 +37,7 @@ int main()
 
     int n; fin>>n;
     cows.resize(n);
-    for (int i=0;i<n;i++) {
+    for (int i=0;i<n;i++) { //IO
         int k; fin>>k;
         for (int j=0;j<k;j++) {
             string trait; fin>>trait;
@@ -46,10 +46,10 @@ int main()
         }
     }
 
-    for (auto &val: sz)
+    for (auto &val: sz) //copy items from map to vector
         order.emplace_back(val.S,val.F);
 
-    sort(order.rbegin(),order.rend());
+    sort(order.rbegin(),order.rend()); //sort vector in reverse
 
     fout << (dfs(cows) ? "yes" : "no") << '\n';
 
